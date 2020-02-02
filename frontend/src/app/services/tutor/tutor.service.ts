6e434ef0f12d374f2189c2ce08826a9969ca616f
vaishakhiification <vaishakhipilankar@gmail.com>
2020-02-02 07:53:07 -0800

    tutor component UI improvement
    Signed-off-by: vaishakhiification <vaishakhipilankar@gmail.com>

--- frontend/src/app/services/tutor/tutor.service.ts
+++ frontend/src/app/services/tutor/tutor.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {SaveAppointment} from '../../models/saveAppointment';
-import { ProfileService } from '../profile/profile.service';
+import {ProfileService} from '../profile/profile.service';
 
 @Injectable({
   providedIn: 'root'
@@ -18,10 +18,11 @@ export class TutorService {
 
   }
 
-  public submitChanges(course_id: string, date: string): Observable<any> {
+  // @ts-ignore
+  public submitChanges(courseId: string, date: string): Observable<any> {
     const localUrl = 'http://169.234.110.139:5000/RegisterForTutor';
     const formData = new FormData();
-    formData.append('course_id', course_id);
+    formData.append('course_id', courseId);
     formData.append('tutor_id', this.profileService.profileDetails.user_id);
     formData.append('appointment', date);
   }
