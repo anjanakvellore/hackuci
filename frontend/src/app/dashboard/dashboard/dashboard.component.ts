47a8ef14eb85b39d435e4f95755a5c207540b04f
Burhanuddin Lakdawala <blakdawa@uci.edu>
2020-02-02 02:02:29 -0800

    fixes to landing page UI

--- frontend/src/app/dashboard/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard/dashboard.component.ts
@@ -10,73 +10,80 @@ import { DashboardService } from '../../services/dashboard/dashboard.service';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(private service:DashboardService) { }
-  public allStudentAppointments:StudentAppointment[];
-  public studentAppointments:StudentAppointment[];
-  public tutorAppointments:TutorAppointment[];
-  public pendingStudentAppointments:StudentAppointment[];
+  constructor(private service: DashboardService) { }
+  public allStudentAppointments: StudentAppointment[];
+  public studentAppointments: StudentAppointment[];
+  public tutorAppointments: TutorAppointment[];
+  public pendingStudentAppointments: StudentAppointment[];
 
   ngOnInit() {
-    this.studentAppointments=[];
-    this.pendingStudentAppointments=[];
+    this.studentAppointments = [];
+    this.pendingStudentAppointments = [];
     this.getAllStudentAppointments();
     this.getTutorAppointments();
   }
 
 
-  getAllStudentAppointments(){
+  getAllStudentAppointments() {
     this.service.getAllStudentAppointments()
     .subscribe(data => {
+      // tslint:disable-next-line:no-debugger
+      debugger;
+      console.log('all appointments', data);
       this.allStudentAppointments = data.transactions;
-      this.studentAppointments = this.allStudentAppointments.filter(x=>x.status=="1");
-      this.studentAppointments.forEach(x=>{
-          var date = new Date(x.appointment);
+      console.log(this.allStudentAppointments);
+      this.studentAppointments = this.allStudentAppointments.filter(x => x.status === 1);
+      console.log(this.studentAppointments);
+      this.studentAppointments.forEach(x => {
+          const date = new Date(x.appointment);
           x.appointment = date.toLocaleString();
       });
-      this.pendingStudentAppointments = this.allStudentAppointments.filter(x=>x.status=="0");
-      this.pendingStudentAppointments.forEach(x=>{
-        var date = new Date(x.appointment);
+      console.log(this.studentAppointments);
+      this.pendingStudentAppointments = this.allStudentAppointments.filter(x => x.status === 0);
+      this.pendingStudentAppointments.forEach(x => {
+        const date = new Date(x.appointment);
         x.appointment = date.toLocaleString();
     });
       console.log(this.pendingStudentAppointments);
     });
   }
 
-  getTutorAppointments(){
+  getTutorAppointments() {
       this.service.getTutorAppointments()
-      .subscribe(data=>{
+      .subscribe(data => {
+        console.log('all tut appointments', data);
         this.tutorAppointments = data.transactions;
-        this.tutorAppointments = this.tutorAppointments.filter(x=>x.status!="2");
-        this.tutorAppointments.forEach(x=>{
-          var date = new Date(x.appointment);
+        this.tutorAppointments = this.tutorAppointments.filter(x => x.status !== '2');
+        this.tutorAppointments.forEach(x => {
+          const date = new Date(x.appointment);
           x.appointment = date.toLocaleString();
-          if(x.status=="1"){
-            x.statusStr = "Accepted";
-          } else if(x.status == "0"){
-            x.statusStr = "Pending";
+          if (x.status === '1') {
+            x.statusStr = 'Accepted';
+          } else if (x.status === '0') {
+            x.statusStr = 'Pending';
           }
       });
-        
+
       });
   }
 
-  onClickAccept(transaction_id:string){
-    console.log("clicked accept"+transaction_id);
-    this.service.sendAppointmentStatus(transaction_id,"1").subscribe(x=>{
-        this.pendingStudentAppointments = this.pendingStudentAppointments.filter(y=>y.transaction_id!=transaction_id);
+  onClickAccept(transaction_id: string) {
+    console.log('clicked accept' + transaction_id);
+    this.service.sendAppointmentStatus(transaction_id, '1').subscribe(x => {
+        this.pendingStudentAppointments = this.pendingStudentAppointments.filter(y => y.transaction_id !== transaction_id);
 
     });
 
   }
 
-  onClickReject(transaction_id:string){
-    console.log("clicked reject"+transaction_id);
-    this.service.sendAppointmentStatus(transaction_id,"2").subscribe(x=>{
-        this.pendingStudentAppointments = this.pendingStudentAppointments.filter(y=>y.transaction_id!=transaction_id);
+  onClickReject(transaction_id: string) {
+    console.log('clicked reject' + transaction_id);
+    this.service.sendAppointmentStatus(transaction_id, '2').subscribe(x => {
+        this.pendingStudentAppointments = this.pendingStudentAppointments.filter(y => y.transaction_id !== transaction_id);
 
     });
 
   }
-    
+
 
 }
